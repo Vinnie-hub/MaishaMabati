0416b9ce5bfe256fd095ddaef713f967b593f3b8
vinnie-hub <vincentmasoso@gmail.com>
2026-09-12 19:26:04 +0300

    update website

--- JS/wishlist.js
+++ JS/wishlist.js
@@ -1,5 +1,5 @@
 /* =========================================================
-   WISHLIST — localStorage
+   WISHLIST — localStorage 
    ========================================================= */
 function getWishlist() {
   try {
